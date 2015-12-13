4570d8bb7f33c05771071572df81ca0de65bafce
hvs-fasya <hvs-fasya@mail.ru>
2015-12-13 18:47:44 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyStack::Application.config.secret_token = '6cf6e0a169c4282d4be969a77da49bf57ed075c87d68522030a605530e9185305feceb0f28422324c19e1d7479ca4545930f759f8961d872532a6a1733e5fbee'
